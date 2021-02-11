88b2b46b7d442e1ebdfc5c970a0f2338a3627558
marton-balazs-kovacs <43272864+marton-balazs-kovacs@users.noreply.github.com>
2021-02-11 19:29:39 +0100

    added already clean code to the vignette

---
+++ R/summarise_study.R
@@ -0,0 +1,22 @@
+#' Function to summarize studies into variables
+#'
+#' Creates a datatable of aggregated values based
+#' on a datatable with the raw variables.
+#'
+#' @param data datatable containing the raw values
+#'
+#' @return The function returns the summarized dataframe.
+#' @export
+summarise_study <- function(data = NULL){
+
+  stopifnot(!is.null(data))
+
+  data %>%
+    summarise(N = n(),
+              `Age Mean` = mean(Age, na.rm = TRUE),
+              `Age SD` = sd(Age, na.rm = TRUE),
+              `Male %` = mean(Gender == 1, na.rm = TRUE),
+              `Higher education %` = mean(`Higher education`, na.rm = TRUE),
+              .groups = "drop")
+
+}
