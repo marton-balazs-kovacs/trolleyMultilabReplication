e51e73c3f184d5b534b7343f5a4fcc853217647a
marton-balazs-kovacs <43272864+marton-balazs-kovacs@users.noreply.github.com>
2021-02-23 09:18:03 +0100

    added infosheet

--- data-raw/trolley_infosheet.R
+++ data-raw/trolley_infosheet.R
@@ -1,31 +1,34 @@
 library(tidyverse)
 library(googlesheets4)
+library(writexl)
 
-authors <- googlesheets4::read_sheet("https://docs.google.com/spreadsheets/d/1hacxT8-8Sd_ABBe_hyw3O4rc_2O7JgErlgW-o3SpdxM/edit?usp=sharing")
+trolley_infosheet_raw <- googlesheets4::read_sheet("https://docs.google.com/spreadsheets/d/1hacxT8-8Sd_ABBe_hyw3O4rc_2O7JgErlgW-o3SpdxM/edit?usp=sharing")
 
-# names_sep <-
-#   infosheet %>%
-#   mutate(Firstname = str_remove(`Full Name as used for publishing`, " .*$"),
-#          `Middle Name` = str_remove(`Full Name as used for publishing`, Firstname),
-#          `Middle Name` = str_remove(`Middle Name`, `Last Name`)) %>%
-#   select(`Full Name as used for publishing`, `Last Name`, Firstname, `Middle Name`)
-
-core_team <- infosheet %>%
+core_team <-
+  trolley_infosheet_raw  %>%
   filter(!is.na(`Order in publication`)) %>%
   mutate(`Core team` = 1L)
 
-not_core_team <- infosheet %>%
+not_core_team <-
+  trolley_infosheet_raw  %>%
   filter(is.na(`Order in publication`)) %>%
   arrange(Surname) %>%
-  mutate(`Order in publication` = 7:245,
+  mutate(`Order in publication` = 7:255,
          `Core team` = 0L,
          Investigation = TRUE) %>%
-  mutate_at(vars(11:14, 16:24), ~ FALSE)
+  mutate_at(
+    vars(11:14, 16:24),
+    ~ case_when(is.na(.) ~ FALSE,
+                !is.na(.) ~ .,
+                TRUE ~ NA))
 
-infosheet_clean <- bind_rows(core_team, not_core_team)
+trolley_infosheet <- bind_rows(core_team, not_core_team)
 
-infosheet_clean <-
-  infosheet_clean %>%
+trolley_infosheet <-
+  trolley_infosheet %>%
+  mutate(`Grant Information` = case_when(`Grant Information` %in% c("No", "none", "None") ~ NA_character_,
+                                         TRUE ~ `Grant Information`)) %>%
   arrange(`Order in publication`)
 
-usethis::use_data(trolley_infosheet, overwrite = TRUE)
+# Save data as external datafile
+write_csv(trolley_infosheet, "inst/extdata/trolley_infosheet.csv")
