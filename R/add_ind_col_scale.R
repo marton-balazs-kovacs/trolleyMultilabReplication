b07a32b46360be8be141be01a78c963a8809e37b
marton-balazs-kovacs <43272864+marton-balazs-kovacs@users.noreply.github.com>
2021-02-23 10:00:18 +0100

    fixed rowwise in add_ind_col_scale.R

--- R/add_ind_col_scale.R
+++ R/add_ind_col_scale.R
@@ -13,8 +13,10 @@
 add_ind_col_scale <- function(df) {
   df %>%
     dplyr::rowwise() %>%
-    dplyr::mutate(hor_ind = mean(c(individualism_scale_1, individualism_scale_2, individualism_scale_3, individualism_scale_4)),
-           ver_ind = mean(c(individualism_scale_5, individualism_scale_6, individualism_scale_7, individualism_scale_8)),
-           hor_col = mean(c(individualism_scale_9, individualism_scale_10, individualism_scale_11, individualism_scale_12)),
-           ver_col = mean(c(individualism_scale_13, individualism_scale_14, individualism_scale_15, individualism_scale_16)))
+    dplyr::mutate(
+      hor_ind = mean(c(individualism_scale_1, individualism_scale_2, individualism_scale_3, individualism_scale_4)),
+      ver_ind = mean(c(individualism_scale_5, individualism_scale_6, individualism_scale_7, individualism_scale_8)),
+      hor_col = mean(c(individualism_scale_9, individualism_scale_10, individualism_scale_11, individualism_scale_12)),
+      ver_col = mean(c(individualism_scale_13, individualism_scale_14, individualism_scale_15, individualism_scale_16))) %>%
+  dplyr::ungroup()
 }
