091ec9d05b49a56cf03a9448413972769348f171
nthun <thomassocska@yahoo.com>
2021-12-16 13:34:34 +0100

    Remove element_blanks() as that is easy to control in the plot

--- R/theme_trolley.R
+++ R/theme_trolley.R
@@ -4,10 +4,9 @@ theme_trolley <- function() {
     panel.grid.major = element_blank(),
     panel.grid.minor = element_blank(),
     plot.title = element_text(hjust = 0.5, vjust = 0.5, face = "bold", size = 17),
-    axis.title.x = element_blank(),
+    axis.title.x = element_text(face = "bold", size = 10),
     axis.title.y = element_text(face = "bold", size = 10),
     axis.text.x = element_text(face = "bold", size = 10, colour = "Black"),
     axis.text.y = element_text(face = "bold", size = 12, colour = "Black"),
-    legend.title = element_blank(),
     legend.text = element_text(size = 9))
   }
