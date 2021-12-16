1aeacea8bd157ad86aae371ce5456c38e5d26879
nthun <thomassocska@yahoo.com>
2021-12-16 13:39:38 +0100

    Change theme, scales, and axes

--- R/create_plot_country.R
+++ R/create_plot_country.R
@@ -17,25 +17,20 @@ create_plot_country <- function(data) {
       ggplot2::aes(
         size = N,
         color = Region)) +
-    ggplot2::guides(size = FALSE) +
+    ggplot2::guides(size = "none") +
     ggplot2::geom_text(hjust = -0.2, size = 3) +
     ggplot2::geom_smooth(
       method = "lm",
       mapping = ggplot2::aes(weight = N),
       color = "blue",
+      alpha = .5,
       show.legend = FALSE,
       se = F) +
-    ggplot2::xlab("Effect size") +
-    ggplot2::scale_y_continuous(breaks = c(0, 0.05, 0.1, 0.15), limits = c(0, 0.15)) +
-    ggplot2::scale_x_continuous(breaks = c(0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8) )+
-    ggplot2::theme_bw() +
-    ggplot2::theme(
-      panel.grid.major = element_blank(),
-      panel.grid.minor = element_blank(),
-      plot.title = element_text(hjust = 0.5, vjust = 0.5, face = "bold", size = 17),
-      axis.title.y = element_text(face = "bold", size = 10),
-      axis.text.x = element_text(face = "bold", size = 10, colour = "Black"),
-      axis.text.y = element_text(face = "bold", size = 12, colour = "Black"),
-      legend.title = element_blank(),
-      legend.text = element_text(size = 9))
+    ggplot2::labs(x = "Cohen's d", y = NULL, color = NULL) +
+    ggplot2::scale_y_continuous(breaks = c(0, 0.05, 0.1, 0.15),
+                                limits = c(0, 0.15)) +
+    ggplot2::scale_x_continuous(breaks = seq(0, .9, .1),
+                                limits = c(0, .9))+
+
+    theme_trolley()
 }
