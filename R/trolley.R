d5dd0b49a5b9171ece6de1c92a674f4534cfc9f6
Marton Kovacs <marton.balazs.kovacs@gmail.com>
2022-02-21 11:11:51 +0100

    updated documentation

---
+++ R/trolley.R
@@ -0,0 +1,109 @@
+#' Codebook for the processed datafile
+#'
+#' This datafile contains the codebook for the processed data of
+#' the PSA006 Trolley multilab replication.
+#'
+#' @format A dataframe with 27502 rows and 152 variables:
+#' \describe{
+#'   \item{survey_name}{character, Either "PSA006_Eastern", "PSA006_Southern", or "PSA006_Western". The distinction is only important because of the materials.}
+#'   \item{survey_id}{character, Unique id corresponding to the survey_name.}
+#'   \item{StartDate}{date, Datetime at the start of the completition of the survey.}
+#'   \item{EndDate}{date, Datetime at the end of the completition of the survey.}
+#'   \item{ResponseId}{character, Unique indetifier of the respondent.}
+#'   \item{trolley_1_resp}{integer, Response for the trolley footbridge pole dilemma. Either 1 for "yes" or 2 for no".}
+#'   \item{trolley_1_rate}{integer, Rating of moral acceptability for the trolley footbridge pole dilemma. Using 9 point Likert-type scale, 1 is "Completely unacceptable" while 9 is "Completely acceptable".}
+#'   \item{trolley_1_just}{character, Justification for the trolley footbridge pole dilemma.}
+#'   \item{trolley_2_resp}{integer, Response for the trolley footbridge switch dilemma. Either 1 for "yes" or 2 for "no".}
+#'   \item{trolley_2_rate}{integer, Rating of moral acceptability for the trolley footbridge switch dilemma. Using 9 point Likert-type scale, 1 is "Completely unacceptable" while 9 is "Completely acceptable".}
+#'   \item{trolley_2_just}{character, Justification for the trolley footbridge switch dilemma.}
+#'   \item{trolley_3_resp}{integer, Response for the trolley standard switch dilemma. Either 1 for "yes" or 2 for "no".}
+#'   \item{trolley_3_rate}{integer, Rating of moral acceptability for the trolley standard switch dilemma. Using 9 point Likert-type scale, 1 is "Completely unacceptable" while 9 is "Completely acceptable".}
+#'   \item{trolley_3_just}{character, Justification for the trolley standard switch dilemma.}
+#'   \item{trolley_4_resp}{integer, Response for the trolley standard footbridge dilemma. Either 1 for "yes" or 2 for "no".}
+#'   \item{trolley_4_rate}{integer, Rating of moral acceptability for the trolley standard footbridge dilemma. Using 9 point Likert-type scale, 1 is "Completely unacceptable" while 9 is "Completely acceptable".}
+#'   \item{trolley_4_just}{character, Justification for the trolley standard footbridge dilemma.}
+#'   \item{trolley_5_resp}{integer, Response for the trolley loop dilemma. Either 1 for "yes" or 2 for "no".}
+#'   \item{trolley_5_rate}{integer, Rating of moral acceptability for the trolley loop dilemma. Using 9 point Likert-type scale, 1 is "Completely unacceptable" while 9 is "Completely acceptable".}
+#'   \item{trolley_5_just}{character, Justification for the trolley loop dilemma.}
+#'   \item{trolley_6_resp}{integer, Response for the trolley obstacle collide dilemma. Either 1 for "yes" or 2 for "no".}
+#'   \item{trolley_6_rate}{integer, Rating of moral acceptability for the trolley obstacle collide dilemma. Using 9 point Likert-type scale, 1 is "Completely unacceptable" while 9 is "Completely acceptable".}
+#'   \item{trolley_6_just}{character, Justification for the trolley obstacle collide dilemma.}
+#'   \item{speedboat_1_resp}{integer, Response for the speedboat footbridge pole dilemma. Either 1 for "yes" or 2 for "no".}
+#'   \item{speedboat_1_rate}{integer, Rating of moral acceptability for the speedboat footbridge pole dilemma. Using 9 point Likert-type scale, 1 is "Completely unacceptable" while 9 is "Completely acceptable".}
+#'   \item{speedboat_1_just}{character, Justification for the speedboat footbridge pole dilemma.}
+#'   \item{speedboat_2_resp}{integer, Response for the speedboat footbridge switch dilemma. Either 1 for "yes" or 2 for "no".}
+#'   \item{speedboat_2_rate}{integer, Rating of moral acceptability for the speedboat footbridge switch dilemma. Using 9 point Likert-type scale, 1 is "Completely unacceptable" while 9 is "Completely acceptable".}
+#'   \item{speedboat_2_just}{character, Justification for the speedboat footbridge switch dilemma.}
+#'   \item{speedboat_3_resp}{integer, Response for the speedboat standard switch dilemma. Either 1 for "yes" or 2 for "no".}
+#'   \item{speedboat_3_rate}{integer, Rating of moral acceptability for the speedboatstandard switch dilemma. Using 9 point Likert-type scale, 1 is "Completely unacceptable" while 9 is "Completely acceptable".}
+#'   \item{speedboat_3_just}{character, Justification for the speedboat standard switch dilemma.}
+#'   \item{speedboat_4_resp}{integer, Response for the speedboat standard footbridge dilemma. Either 1 for "yes" or 2 for "no".}
+#'   \item{speedboat_4_rate}{integer, Rating of moral acceptability for the speedboat standard footbridge dilemma. Using 9 point Likert-type scale, 1 is "Completely unacceptable" while 9 is "Completely acceptable".}
+#'   \item{speedboat_4_just}{character, Justification for the speedboat standard footbridge dilemma.}
+#'   \item{speedboat_5_resp}{integer, Response for the speedboat loop dilemma. Either 1 for "yes" or 2 for "no".}
+#'   \item{speedboat_5_rate}{integer, Rating of moral acceptability for the speedboat loop dilemma. Using 9 point Likert-type scale, 1 is "Completely unacceptable" while 9 is "Completely acceptable".}
+#'   \item{speedboat_5_just}{character, Justification for the speedboat loop dilemma.}
+#'   \item{speedboat_6_resp}{integer, Response for the speedboat obstacle collide dilemma. Either 1 for "yes" or 2 for "no".}
+#'   \item{speedboat_6_rate}{integer, Rating of moral acceptability for the speedboat obstacle collide dilemma. Using 9 point Likert-type scale, 1 is "Completely unacceptable" while 9 is "Completely acceptable".}
+#'   \item{speedboat_6_just}{character, Justification for the speedboat obstacle collide dilemma.}
+#'   \item{trolley_attention}{integer, Response to the trolley attention check question. Respondent had to choose the description of the presented trolley dilemma. Correct responses can be found in the package system data at "R/sysdata.rda" named "correct_answers".}
+#'   \item{speedboat_attention}{integer, Response to the speedboat attention check question. Respondent had to choose the description of the presented speedboat dilemma. Correct responses can be found in the package system data at "R/sysdata.rda" named "correct_answers".}
+#'   \item{confusion}{integer, Wheter the respondent found the materials confusing. 1 is "I did not find the material on the preceding pages confusing.", 2 is "I found some of the material on the preceding pages confusing at first, but after reading the material carefully and examining the diagram I felt that I understood the material well enough to give reasonable answers to the questions I was asked.", while 3 is "By the time I answered the questions on the preceding page, I was still somewhat confused by the material. I do not think I understood this material well enough to give reasonable answers to the questions I was asked.".}
+#'   \item{disbelief}{integer, Whether the respondent found the materials believable. 1 is "I found the description from the preceding pages to be rather realistic.", 2 is "I did not find the description from the preceding pages to be completely realistic, but I “suspended disbelief” and responded under the assumption that it was completely accurate. More specifically, I assumed that Joe’s (or John's) action (or inaction) would affect the situation as the description said it would.", and 3 is "I did not find the description from the preceding pages to be realistic, and my answers reflect my inability to take seriously the description that was given.".}
+#'   \item{familiarity}{integer, Familiarity with the task from 1 to 5, where 1 is "absolutely not familiar" and 5 is "absolutely familiar".}
+#'   \item{careless_1}{integer, Response to question to the first question to check carelessness. Correct response 2.}
+#'   \item{careless_2}{integer, Response to question to the second question to check carelessness. Correct response 2.}
+#'   \item{careless_3}{integer, Response to question to the third question to check carelessness. Correct response 1.}
+#'   \item{household_income}{numeric, Household income of the respondent in 2019.}
+#'   \item{currency_1}{character, Currency of the participants income.}
+#'   \item{living_area}{character, Living area of the respondent where 1 is "Rural" and 2 is "Urban".}
+#'   \item{oxford_utilitarian_1}{integer, First item of the Oxford utilitarian scale. 1 is "Strongly disagree" and 7 is "Strongly agree".}
+#'   \item{individualism_scale_1}{integer, First item of the Individualism scale. 1 is "Never of definitely no" while 9 is "Always or definitely yes".}
+#'   \item{religious_rate}{integer, How religious is the respondent from 1 "absolutely not" to 10 "very".}
+#'   \item{religion_1}{integer, Whether the respondent indicated that following the first listed religion. For the listed religions see the projects OSF page at https://osf.io/efy2w/.}
+#'   \item{education_leve}{integer, Highest level of education. 1 is "Below high school", 2 is "High school", 3 is "Bachelor's degree", 4 is "Master's degree", 5 is "Doctoral degree". The labels can differ for certian countries but these are recorded in separate variables.}
+#'   \item{age_1}{integer, Age of the respondent, minimum 18.}
+#'   \item{sex}{integer, Gender of the respondent.}
+#'   \item{countr_origin_1}{character, Origin country of the respondent.}
+#'   \item{immigrant_back}{integer, Is the respondent from an immigrant background, with 1 "Yes" and 2 "No".}
+#'   \item{technical_problems}{integer, Whether the respondent had technical problems during survey completition. 1 is "No" while 2 is "Yes".}
+#'   \item{technical_problems_2_TEXT}{character, Description of the technical problem.}
+#'   \item{further_comments}{character, Further comments by the respondent.}
+#'   \item{native_language}{integer, Was the study presented in the primary language of the respondent with 1 as "Yes" and 2 as "No"}
+#'   \item{fluency}{integer, Is the respondent fluent in the language of the survey with 1 as "Yes" and 2 as "No".}
+#'   \item{Q_Lang}{character, Language of the survey.}
+#'   \item{lab}{character, Unique identifier of the lab collecting the given response.}
+#'   \item{attention}{, }
+#'   \item{scenario1}{character, Which dilemma was shown to the respondent first. Added during data cleaning.}
+#'   \item{scenario2}{character, Which dilemma was shown to the respondent second. Added during data cleaning.}
+#'   \item{country3}{character, ISO3 country code of the respondents' country.}
+#'   \item{Region}{character, Which world region the respondents' country belongs to.}
+#'   \item{Age}{integer, Recoded age value during data cleaning.}
+#'   \item{Gender}{character, Recoded values of the "sex" variable during data cleaning.}
+#'   \item{Higher education}{character, Recoded values of the "education_leve" variable during data cleaning.}
+#'   \item{include_nocareless}{logical, TRUE if respondent passed the carelesness test and FALSE if dont}
+#'   \item{include_noconfusion}{logical, TRUE if respondent passed the confusion test and FALSE if dont}
+#'   \item{include_nofamiliarity}{logical, TRUE if respondent was not familiar with the task and FALSE if they were}
+#'   \item{include_notechproblem}{logical, TRUE if respondent had no technical problem and FALSE if they had}
+#'   \item{include_nonativelang}{logical, TRUE if respondent filled out the survey in their native language and FALSE if dont}
+#'   \item{include_noproblem}{logical, TRUE if respondent passed all the exclusion criteria (i.e. carelessness, confusion, familiarity, technical probelem, and native language check) and FALSE if dont}
+#'   \item{include_withoutfamiliarity}{logical, TRUE if respondent passed all exclusion criteria except familiarity does not matter and FALSE if dont}
+#'   \item{include_familiar}{logical, TRUE if respondent passed all exclusion criteria but only including respondents who were familiar with the tasks and FALSE if dont}
+#'   \item{include_study1a_attention}{logical, TRUE if respondent passed the attention check for study1a but non of the other attention check applied and FALSE if dont}
+#'   \item{include_study1b_attention}{logical, TRUE if respondent passed the attention check for study1b but non of the other attention check applied and FALSE if dont}
+#'   \item{include_study1a}{logical, TRUE if respondent passed all of the checks including the attention check for study1a and FALSE if dont}
+#'   \item{include_study1b}{logical, TRUE if respondent passed all of the checks including the attention check for study1b and FALSE if dont}
+#'   \item{include_study1a_withoutfamiliarity}{logical, TRUE if respondent passed all of the checks including the attention check for study1a but familiarity does not matter and FALSE if dont}
+#'   \item{include_study1b_withoutfamiliarity}{logical, TRUE if respondent passed all of the checks including the attention check for study1b but familiarity does not matter and FALSE if dont}
+#'   \item{include_study1a_familiar}{logical, TRUE if respondent passed all of the checks including the attention check for study1a but only including respondents familiar with the task and FALSE if dont}
+#'   \item{include_study1b_familiar}{logical, TRUE if respondent passed all of the checks including the attention check for study1b but only including respondents familiar with the task and FALSE if dont}
+#'   \item{include_study2a_attention}{logical, TRUE if respondent passed the attention check for study2a but non of the other attention check applied  and FALSE if dont}
+#'   \item{include_study2b_attention}{logical, TRUE if respondent passed the attention check for study1b but non of the other attention check applied  and FALSE if dont}
+#'   \item{include_study2a}{logical, TRUE if respondent passed all of the checks including the attention check for study2a and FALSE if dont}
+#'   \item{include_study2b}{logical, TRUE if respondent passed all of the checks including the attention check for study2b and FALSE if dont}
+#'   \item{include_study2a_withoutfamiliarity}{logical, TRUE if respondent passed all of the checks including the attention check for study2a but familiarity does not matter and FALSE if dont}
+#'   \item{include_study2b_withoutfamiliarity}{logical, TRUE if respondent passed all of the checks including the attention check for study2b but familiarity does not matter and FALSE if dont}
+#'   \item{include_study2a_familiar}{logical, TRUE if respondent passed all of the checks including the attention check for study2a but only including respondents familiar with the task and FALSE if dont}
+#'   \item{include_study2b_familiar}{logical, TRUE if respondent passed all of the checks including the attention check for study2b but only including respondents familiar with the task and FALSE if dont}
+#'}
+"trolley"
--- README.md
+++ README.md
@@ -6,7 +6,10 @@
 <!-- badges: start -->
 <!-- badges: end -->
 
-The goal of trolleyMultilabReplication is to provide
+The goal of **trolleyMultilabReplication** is to provide a structure for
+the data cleaning and analysis of the PSA006 trolley multilab
+replication project. You can find out more about the project at the
+projects’ [OSF repository](https://osf.io/efy2w/).
 
 ## Installation
 
@@ -18,11 +21,58 @@ The package is currently not on CRAN, but the development version from
 devtools::install_github("marton-balazs-kovacs/trolleyMultilabReplication")
 ```
 
-## Example
+## Description of the package
 
-This is a basic example which shows you how to solve a common problem:
+The package includes high-level data cleaning and and data analyzing
+functions for the project. The functions are moderately documented. The
+documentation will be expanded in the future. To check the documentation
+of a given function use the following format
+`?trolleyMultilabReplication::calculate_study1_stat`.
+
+### Data
+
+The code for data cleaning can be found in the ‘data-raw/’ folder of the
+package. The data wrangling for the source data imported directly from
+Qualtrics can be found in the ‘data-raw/trolley_raw.R’ file.
+
+The raw datafile for the replication that contains the data from all of
+the data collection sites can be found in a .csv format in the
+‘extdata/’ folder and called ‘trolley_raw.csv’.
+
+You can load this datafile if you are replicating the results of the
+study by the following code:
 
 ``` r
-library(trolleyMultilabReplication)
-## basic example code
+trolley_raw <- system.file("extdata", "trolley_raw.csv", package = "trolleyMultilabReplication")
 ```
+
+The steps for preprocessing the raw datafile can be found in the
+‘raw-data/trolley_preprocessed.R’ file. The code for creating the final
+processed datafile can be found in the ‘data-raw/trolley.R’ file.
+
+The codebook for the processed datafile can be found by running the
+following function: `?trolleyMultilabReplication::trolley`. This
+codebook contains the description of the most important variables for
+the raw data as well.
+
+The preprocessed data, the processed data, and the datafile containing
+the cultural distance scores for each country are saved as package data
+files and can be loaded by the following functions:
+
+``` r
+# List the datafiles saved as package data
+data(package = "trolleyMultilabReplication")
+# Cultural distance data
+trolleyMultilabReplication::cultural_distance
+# Preprocessed data
+trolleyMultilabReplication::trolley_preprocessed
+# Processed data
+trolleyMultilabReplication::trolley
+```
+
+### Analysis code
+
+The analysis code that we used for this project including the code for
+the creations of the tables and figures can be found in the ‘vignettes’
+folder. The figures for the project in .pdf format can be found in the
+‘vignettes/figures/’ folder.
